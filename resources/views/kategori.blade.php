525e4beedae64796f6e3e81e871e23dd96436ef6
enjelianna <angeliarazali@gmail.com>
2024-09-13 15:39:51 +0700

    (JS3) Praktikum 6_Implementasi Eloquent ORM

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -8,8 +8,8 @@
     <table border="1" cellpadding="2", cellspacing="0">
         <tr>
             <th>ID</th>
-            <th>Kode Level</th>
-            <th>Nama Level</th>
+            <th>Kode Kategori</th>
+            <th>Nama Kategori</th>
         </tr>
         @foreach ($data as $d )
         <tr>
