12cf42f5c63cc0469c08b88558747627631d6ee2
enjelianna <angeliarazali@gmail.com>
2024-09-20 08:55:16 +0700

    (JS4) Praktikum 2.3_Retreiving Aggregrates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,41 +6,63 @@ use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
-
 class UserController extends Controller
 {
-    public function index()
-    {
-        //prak 1
-    /*    $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-    ];
+    public function index(){
+
+        //JS 4 prak 2.1 no 6
+        // $user = UserModel::firstwhere('level_id', 1);
+
+        //JS 4 prak 2.1 no 8
+        /*$user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });*/
+
+        //JS 4 prak 2.2 no 1
+        // $user = UserModel::findOrFail(1);
+
+        //JS 4 prak 2.2 no 3
+       // $user = UserModel::where('username', 'manager9')->firstOrFail();
     
-$data = [
+        // JS 4 prak 2.3 no 1
+        // $user = UserModel::where('level_id', 2)->count();
+        // dd($user);
+
+        // JS 4 prak 2.3 no 3
+        $user = UserModel::where('level_id', 2)->count();
+        return view('user', ['data' => $user]);
+
+        //JS 4 prak 1
+        /*
+       $data = [
         'level_id' => 2,
         'username' => 'manager_tiga',
         'nama' => 'Manager 3',
         'password' => Hash::make('12345')
-];
-UserModel::create($data);
+       ];
+       UserModel::create($data);
+        */
+      
+      //coba akses model UserModel
+        //$user = UserModel::all(); //Ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+         //JS 3
+                // tambah data user dengan Eloquent Model
+        /*$data = [
+            'username' => 'customer-1',
+            'nama'  => 'Pelanggan',
+            'password' => Hash::make('12345'),
+            'level_id' => 4
+        ];
+        UserModel::insert($data); //tambahkan data ke tabel m_user
         */
 
-        /*prak 2.1
-        $user = UserModel::findOr(20, ['username', 'nama'], function () {
-            abort(404);
-        });
-        return view('user', ['data' => $user]);
-        */
-
-        // prak 2.2
-        $user = UserModel::where('username', 'manager9')->firstOrFail();
-        return view('user', ['data' => $user]);
+        /*$data = [
+            'nama'  => 'Pelanggan Pertama',
+        ];
+        UserModel::where('username', 'customer-1')->update($data); //update data user
+       */
+         
     }
 }
-    
