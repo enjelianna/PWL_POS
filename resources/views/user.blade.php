af7d015bd13879da57e0995c381f88beeea52df4
enjelianna <angeliarazali@gmail.com>
2024-09-20 12:29:05 +0700

    (JS4) Praktikum 2.6_Create, Read, Update, Delete (CRUD)

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,26 +1,70 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Data User</title>
+        <title>
+            Data User
+        </title>
     </head>
-
     <body>
-        <h1>Data User</h1>
+        <h1>
+            Data User
+        </h1>
+        <a href="{{url('/user/tambah')}}"> Tambah User</a>
         <table border="1" cellpadding="2" cellspacing="0">
             <tr>
                 <th>ID</th>
                 <th>Username</th>
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
+                <th>Aksi</th>
             </tr>
-            {{-- @foreach($users as $data) --}}
-            <tr>
-                <td>{{ $data->user_id }}</td>
-                <td>{{ $data->username }}</td>
-                <td>{{ $data->nama }}</td>
-                <td>{{ $data->level_id }}</td>
-            </tr>
-            {{-- @endforeach --}}
+            @foreach ($data as $d)
+                <tr>
+                    <td>{{ $d->user_id }}</td>
+                    <td>{{ $d->username }}</td>
+                    <td>{{ $d->nama }}</td>
+                    <td>{{ $d->level_id }}</td>
+                    <td><a href="{{ url('/user/ubah/' . $d->user_id) }}">Ubah</a> | <a href="{{url('/user/hapus/' .$d->user_id)}}">Hapus</a></td>
+                </tr>
+            @endforeach
         </table>
     </body>
 </html>
+
+{{-- praktikum 2.3 --}}
+
+{{-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data User</title>
+    <style>
+        .data-container {
+            border: 1px solid blue;
+            width: 200px;
+            padding: 10px;
+        }
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+    </style>
+</head>
+<body>
+    <div class="data-container">
+        <h1>Data User</h1>
+        <table>
+            <tr>
+                <th>Jumlah Pengguna</th>
+            </tr>
+            <tr>
+                <td>{{ $data }}</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html> --}}
--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,25 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function() {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/level', [LevelController::class, 'index']);
-Route::get('/kategori', [KategoriController::class, 'index']);
+Route::get('/level',[LevelController::class, 'index']);
+Route::get('/kategori',[KategoriController::class, 'index']);
 Route::get('/user', [UserController::class, 'index']);
 
+//tambahUser
+Route::get('/user/tambah', [UserController::class, 'tambah']);
+
+//simpanUser
+Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
+
+//ubahUser
+Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
+
+//ubahSimpan
+Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubah_simpan']);
+
+//hapusUser
+Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
