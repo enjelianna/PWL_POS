31611ebc164d785eb23ca8f9052bb7eab527fd70
enjelianna <angeliarazali@gmail.com>
2024-09-27 09:15:08 +0700

    (JS5) Praktikum 2_Penerapan Layouting

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,39 +3,39 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>{{ config('app.name', 'PWL Laravel Starter Code')}}</title>
+  <title>{{config ('app.name', 'PWL Laravel Starter Code')}} </title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.cs') }}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('adminlte/dist/css/adminlte.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('adminlte/dist/css/adminlte.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini">
 <!-- Site wrapper -->
 <div class="wrapper">
-  
-  @include('layouts.header')
+
   <!-- /.navbar -->
+@include('layouts.header')
 
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PWL Starter  Code</span>
+    <a href="{{url('/') }}" class="brand-link">
+      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">PWL - Starter Code</span>
     </a>
 
     <!-- Sidebar -->
-   @include('layouts.sidebar') 
+    @include('layouts.sidebar')
     <!-- /.sidebar -->
   </aside>
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
-    @include('layouts.breadcrummb')
+    @include('layouts.breadcrumb')
 
     <!-- Main content -->
     <section class="content">
@@ -45,15 +45,15 @@
   </div>
   <!-- /.content-wrapper -->
 
-  @include('layouts.footer')
+ @include('layouts.footer')
 </div>
 <!-- ./wrapper -->
 
 <!-- jQuery -->
-<script src="{{asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<script src="{{ asset('adminlte/plugins/jquery/jquery.min.js')}}"></script>
 <!-- Bootstrap 4 -->
-<script src="{{asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{ asset('adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
-<script src="{{aseet('adminlte/dist/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('adminlte/dist/js/adminlte.min.js')}}"></script>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,21 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,19 +23,12 @@ Route::get('/', function () {
 
 Route::get('/level',[LevelController::class, 'index']);
 Route::get('/kategori',[KategoriController::class, 'index']);
-Route::get('/user', [UserController::class, 'index']);
+Route::get('/user',[UserController::class, 'index']);
+Route::get('/user/tambah',[UserController::class, 'tambah']);
+Route::post('/user/tambah_simpan',[UserController::class, 'tambah_simpan']);
+Route::get('/user/ubah/{id}',[UserController::class, 'ubah']);
+Route::put('/user/ubah_simpan/{id}',[UserController::class, 'ubah_simpan']);
+Route::get('/user/hapus/{id}',[UserController::class, 'hapus']);
 
-//tambahUser
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-
-//simpanUser
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-
-//ubahUser
-Route::get('/user/ubah/{id}', [UserController::class, 'ubah']);
-
-//ubahSimpan
-Route::put('/user/ubah_simpan/{id}', [UserController::class,'ubah_simpan']);
-
-//hapusUser
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+//Jobsheet 5
+Route::get('/', [WelcomeController::class,'index']);
