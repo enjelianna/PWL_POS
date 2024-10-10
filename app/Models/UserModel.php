1aba716b7f4bbe756bf5389d5d9c0d82cca66bcd
enjelianna <angeliarazali@gmail.com>
2024-10-10 20:45:08 +0700

    (JS7) Tugas 1_Implementasi Authentication

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,19 +5,53 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Foundation\Auth\User as Authenticatable; //implement class Authenticatable
+
+// use App\Models\LevelModel;
+
+use Monolog\Level;
+
+// class UserModel extends Model
+// {
+//     use HasFactory;
+
+//     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
+//     protected $primaryKey = 'user_id';  //Mendefinisikan primary key dati tabel yang digunakan yang digunakan
+//     /**
+//      * The attributes that are mass asigniable
+//      * @var array
+//      */
+//     // protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+//     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+//     public function level(): BelongsTo
+//     {
+//         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+//     } 
+// }
+
 
 class UserModel extends Authenticatable
 {
     use HasFactory;
 
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $primaryKey = 'user_id'; //Mendefiniskan primary key dari tabel yang digunakan
-    protected $fillable = ['username','password','nama','level_id', 'created_at', 'updated_at'];
-    protected $hidden = ['password'];
-    protected $casts=  ['password'=> 'hashed'];
+    protected $primaryKey = 'user_id';  //Mendefinisikan primary key dati tabel yang digunakan yang digunakan
+    /**
+     * The attributes that are mass asigniable
+     * @var array
+     */
 
-    public function level(): BelongsTo{
+    protected $fillable = [ 'username', 'password', 'nama', 'level_id', 'created_at', 'updated_at'];
+
+    protected $hidden = ['password']; //jangan ditampilkan saat select
+
+    protected $casts = ['password' => 'hashed']; //casting password agar otomatis di hash
+
+    public function level(): BelongsTo
+    {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
-    }
+    } 
 }
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,84 +1,100 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-  <!-- Brand Logo -->
-  <a href="{{ url('/') }}" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-          class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">PWL - Starter Code</span>
-  </a>
-
-  <!-- Sidebar -->
-  <div class="sidebar">
-      <!-- SidebarSearch Form -->
-      <div class="form-inline mt-2">
-          <div class="input-group" data-widget="sidebar-search">
-              <input class="form-control form-control-sidebar" type="search" placeholder="Search"
-                  aria-label="Search">
-              <div class="input-group-append">
-                  <button class="btn btn-sidebar">
-                      <i class="fas fa-search fa-fw"></i>
-                  </button>
-              </div>
-          </div>
+<div class="sidebar">
+    {{-- <!-- Sidebar user (optional) -->
+    <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+      <div class="image">
+        <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
       </div>
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
-              data-accordion="false">
-              <li class="nav-item">
-                  <a href="{{ url('/') }}" class="nav-link {{ $activeMenu == 'dashboard' ? 'active' : '' }} ">
-                      <i class="nav-icon fas fa-tachometer-alt"></i>
-                      <p>Dashboard</p>
-                  </a>
-              </li>
-              <li class="nav-header">Data Pengguna</li>
-              <li class="nav-item">
-                  <a href="{{ url('/level') }}" class="nav-link {{ $activeMenu == 'level' ? 'active' : '' }} ">
-                      <i class="nav-icon fas fa-layer-group"></i>
-                      <p>Level User</p>
-                  </a>
-              </li>
-              <li class="nav-item">
-                  <a href="{{ url('/user') }}" class="nav-link {{ $activeMenu == 'user' ? 'active' : '' }}">
-                      <i class="nav-icon far fa-user"></i>
-                      <p>Data User</p>
-                  </a>
-              </li>
-              <li class="nav-header">Data Barang</li>
-              <li class="nav-item">
-                  <a href="{{ url('/kategori') }}" class="nav-link {{ $activeMenu == 'kategori' ? 'active' : '' }} ">
-                      <i class="nav-icon far fa-bookmark"></i>
-                      <p>Kategori Barang</p>
-                  </a>
-              </li>
-              <li class="nav-item">
-                  <a href="{{ url('/supplier') }}" class="nav-link {{ $activeMenu == 'supplier' ? 'active' : '' }} ">
-                      <i class="nav-icon far fa-building"></i>
-                      <p>Supplier Barang</p>
-                  </a>
-              </li>
-              <li class="nav-item">
-                  <a href="{{ url('/barang') }}" class="nav-link {{ $activeMenu == 'barang' ? 'active' : '' }} ">
-                      <i class="nav-icon fa fa-ship"></i>
-                      <p>Data Barang</p>
-                  </a>
-              </li>
-              <li class="nav-header">Data Transaksi</li>
-              <li class="nav-item">
-                  <a href="{{ url('/stok') }}"
-                      class="nav-link {{ $activeMenu == 'stok' ? 'active' : '' }} ">
-                      <i class="nav-icon fas fa-cubes"></i>
-                      <p>Stok Barang</p>
-                  </a>
-              </li>
-              <li class="nav-item">
-                  <a href="{{ url('/barang') }}"
-                      class="nav-link {{ $activeMenu == 'penjualan' ? 'active' : '' }} ">
-                      <i class="nav-icon fas fa-cash-register"></i>
-                      <p>Transaksi Penjualan</p>
-                  </a>
-              </li>
-          </ul>
-      </nav>
-  </div>
-  <!-- /.sidebar -->
-</aside>
+      <div class="info">
+        <a href="#" class="d-block">Alexander Pierce</a>
+      </div>
+    </div> --}}
+
+    <!-- SidebarSearch Form -->
+    <div class="form-inline">
+      <div class="input-group" data-widget="sidebar-search">
+        <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+        <div class="input-group-append">
+          <button class="btn btn-sidebar">
+            <i class="fas fa-search fa-fw"></i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <!-- Sidebar Menu -->
+    <nav class="mt-2"> 
+      <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" 
+  role="menu" data-accordion="false"> 
+        <li class="nav-item"> 
+          <a href="{{ url('/') }}" class="nav-link  {{ ($activeMenu == 'dashboard')? 
+  'active' : '' }} "> 
+            <i class="nav-icon fas fa-tachometer-alt"></i> 
+            <p>Dashboard</p> 
+          </a> 
+        </li> 
+  
+            <li class="nav-header">Data Pengguna</li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/level') }}" class="nav-link {{ ($activeMenu == 'level')? 
+      'active' : '' }} "> 
+                <i class="nav-icon fas fa-layer-group"></i> 
+                <p>Level User</p> 
+              </a> 
+            </li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/user') }}" class="nav-link {{ ($activeMenu == 'user')? 
+      'active' : '' }}"> 
+                <i class="nav-icon far fa-user"></i> 
+                <p>Data User</p> 
+              </a> 
+            </li> 
+            <li class="nav-header">Data Barang</li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/kategori') }}" class="nav-link {{ ($activeMenu == 
+      'kategori')? 'active' : '' }} "> 
+                <i class="nav-icon far fa-bookmark"></i> 
+                <p>Kategori Barang</p> 
+              </a> 
+            </li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu == 
+      'barang')? 'active' : '' }} "> 
+                <i class="nav-icon far fa-list-alt"></i> 
+                <p>Data Barang</p> 
+              </a> 
+            </li> 
+            <li class="nav-header">Data Transaksi</li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/stok') }}" class="nav-link {{ ($activeMenu == 'stok')? 
+      'active' : '' }} "> 
+                <i class="nav-icon fas fa-cubes"></i> 
+                <p>Stok Barang</p> 
+              </a> 
+            </li> 
+            <li class="nav-item"> 
+              <a href="{{ url('/barang') }}" class="nav-link {{ ($activeMenu == 
+      'penjualan')? 'active' : '' }} "> 
+                <i class="nav-icon fas fa-cash-register"></i> 
+                <p>Transaksi Penjualan</p> 
+              </a> 
+            </li> 
+            <li class="nav-header">Data Supplier</li>
+            <li class="nav-item"> 
+                <a href="{{ url('/supplier') }}" class="nav-link {{ ($activeMenu == 'supplier') ? 'active' : '' }}"> 
+                    <i class="nav-icon fas fa-truck"></i> 
+                    <p>Data Supplier</p> 
+                </a> 
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('logout') }}" class="nav-link"
+                 onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                <i class="nav-icon fas fa-sign-out-alt"></i>
+                <p>Logout</p>
+              </a>
+              <form id="logout-form" action="{{ url('logout') }}" method="POST" style="display: none;">
+                @csrf
+              </form>
+            </li>
+          </ul> 
+        </nav> 
+      </div> 
