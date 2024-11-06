5f9164941584e2baef3dff5829481a54c8a975e4
enjelianna <angeliarazali@gmail.com>
2024-11-06 14:03:54 +0700

    (JS11) Praktikum 1_Implementasi Eloquent Accessor

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,40 +2,65 @@
 
 namespace App\Models;
 
-use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
+
+// class UserModel extends Model
 class UserModel extends Authenticatable implements JWTSubject
 {
-    public function getJWTIdentifier(){
-        return $this->getkey();
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
     }
-        public function getJWTCustomClaims(){
+    public function getJWTCustomClaims()
+    {
         return [];
     }
 
-
     protected $table = 'm_user'; //Mendefinisikan nama tabel yang digunakan oleh model ini
     protected $primaryKey = 'user_id'; //Mendefiniskan primary key dari tabel yang digunakan
-    protected $fillable = ['username','password','nama','level_id', 'created_at', 'updated_at'];
-    // protected $hidden = ['password'];
-    protected $casts=  ['password'=> 'hashed'];
-
-    public function level(): BelongsTo{
-        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    protected $fillable = [
+        'username',
+        'nama',
+        'password',
+        'level_id',
+        'image'//tambahan
+    ];
+    
+    public function level()
+    {
+        return $this->belongsTo( LevelModel::class, 'level_id', 'level_id');
+    }
+    protected function image(): Attribute
+    {
+        return Attribute::make(
+            get: fn($image) => url('/storage/posts/' . $image),
+        );
     }
 
-    public function getRoleName(): string{
-        return $this->level->level_nama;
-    }
-    public function hasRole($role): bool{
-        return $this->level->level_kode == $role;
-    }
-    public function getRole(){
-        return $this->level->level_kode;
-    }
+    // public function profil(): HasOne
+    // {
+    //     return $this->hasOne(ProfilUserModel::class, 'user_id', 'user_id');
+    // }
+
+    // public function getRoleName(): string
+    // {
+    //     return $this->level->level_nama;
+    // }
+
+    // public function hasRole($role): bool
+    // {
+    //     return $this->level->level_kode == $role;
+    // }
+
+    // public function getRole()
+    // {
+    //     return $this->level->level_kode;
+    // }
 }
